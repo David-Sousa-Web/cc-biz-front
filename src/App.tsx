12258cff1ce5fd402661c2c63c70e67a180ab993
David Sousa <david.sousa@bizsys.com.br>
2025-05-19 18:42:53 -0300

    fix: location icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { ToastContainer } from "react-toastify";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './styles/App.css';
 import './styles/root.css';
 
--- src/components/layout/location.tsx
+++ src/components/layout/location.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LocationNotice: React.FC = () => (
   <div className="location-notice">
-    <i className="bi bi-geo-alt"></i>
+    <i className="bi bi-geo-alt-fill"></i>
     <p>
       <strong>Atenção!</strong> É importante que o localizador do seu celular
       esteja ativado.
