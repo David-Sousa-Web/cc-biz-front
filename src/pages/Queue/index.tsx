84711050f17f26ba9c9239bc840b743120698448
David Sousa <david.sousa@bizsys.com.br>
2025-05-21 18:26:53 -0300

    fix: comment

--- src/pages/Queue/index.tsx
+++ src/pages/Queue/index.tsx
@@ -4,7 +4,7 @@ import { getFirstUserPosition } from "../../utils/getUserPositions";
 import { useNavigate } from "react-router-dom";
 import '../../styles/queue.css'
 
-const REFRESH_INTERVAL = 5000; // 10 segundos em ms
+const REFRESH_INTERVAL = 5000; // 5 segundos em ms
 
 export default function Queue() {
   const [position, setPosition] = useState<number | null>(null);
